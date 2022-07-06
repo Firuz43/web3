58000af03d17685fda31cfc1d003b09844256f92
Firuz43 <firuz1199@mail.ru>
2022-07-06 05:53:16 +0400

    js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// console.log("Hello there");
+// let x = "Volvo" + 12;
+// console.log(x);
+
+// let number = calculate(4, 5);
+
+// function calculate(a, b) {
+//   return a + b;
+// }
+
+// console.log(number);
+
+// let square = calcPerimetr(5);
+
+// function calcPerimetr(a) {
+//   return a * a;
+// }
+
+// console.log(square);
+
+// const car = { firstName: "Bmw", weight: 500, color: "black" };
+
+// console.log((car.firstName = "FFF"));
+
+// let clicks = 0;
+// function addCount() {
+//   clicks += 1;
+//   document.getElementById("demo").innerHTML = clicks;
+// }
+
+let text = "I'm goin to see Firuz";
+
+console.log(text.indexOf("going"));
